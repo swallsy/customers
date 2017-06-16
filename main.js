2deaab31187c3e672b0cfddb4d9c4b8d9c42c55e
swallsy <smwall84@gmail.com>
2017-06-16 11:10:00 -0400

    changes data retrieval to retrieve all 12 at once

--- main.js
+++ main.js
@@ -1,57 +1,46 @@
 (function () {
 
 'use strict';
+fetch('https://randomuser.me/api/?results=12')
+  .then(
 
-let num = 0;
-  let users = [];
-  while (num < 12) {
-    fetch('https://randomuser.me/api/')
-      .then(
-        function(response) {
-          if (response.status !== 200) {
-            console.log('Looks like there was a problem. Status Code: ' + response.status);
-            return;
-          }
-          // check the response data...
-          response.json().then(function(data) {
-            let user = data.results[0];
-            users.push(user);
-            let photo = data.results[0].picture.large;
-            let firstName = data.results[0].name.first;
-            let lastName = data.results[0].name.last;
-            let email = data.results[0].email;
-            let street = data.results[0].location.street;
-            let city = data.results[0].location.city;
-            let state = data.results[0].location.state;
-            let postcode = data.results[0].location.postcode;
-            let phone = data.results[0].phone;
-            let ss = data.results[0].id.value;
-            console.log(firstName);
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' + response.status);
+        return;
+      } else
+      response.json().then(function(data) {
+      // check the response data...
+      for(let i = 0; i < data.results.length; i++) {
+        let user = data.results[i];
+        let photo = data.results[i].picture.large;
+        let firstName = data.results[i].name.first;
+        let lastName = data.results[i].name.last;
+        let email = data.results[i].email;
+        let street = data.results[i].location.street;
+        let city = data.results[i].location.city;
+        let state = data.results[i].location.state;
+        let postcode = data.results[i].location.postcode;
+        let phone = data.results[i].phone;
+        let ss = data.results[i].id.value;
 
-            let wrapper = document.getElementById('wrapper');
+        let wrapper = document.getElementById('wrapper');
 
-            let markup =  `
-              <div class="person">
-                <img src="${photo}"></img>
-                <h3>${firstName} ${lastName}</h3>
-                <p class="email">${email}</p>
-                <p class="street">${street}</p>
-                <p class="city">${city} ${state} ${postcode}</p>
-                <p class="phone">${phone}</p>
-                <p class="ss">${ss}</p>
-              </div>
-            `
-            wrapper.innerHTML += markup;
-          }
-        )
+        let markup =  `
+          <div class="person">
+            <img src="${photo}"></img>
+            <h3>${firstName} ${lastName}</h3>
+            <p class="email">${email}</p>
+            <p class="street">${street}</p>
+            <p class="city">${city} ${state} ${postcode}</p>
+            <p class="phone">${phone}</p>
+            <p class="ss">${ss}</p>
+          </div>
+        `
+        wrapper.innerHTML += markup;
+        }
       }
     )
-    num++;
-  }
-  console.log(users);
-
-    for (let i = 0; i < users.length; i++) {
-
-
-    }
+   }
+  )
 })();
